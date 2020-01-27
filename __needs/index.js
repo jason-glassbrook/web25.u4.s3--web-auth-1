b3a6128446958b50c8fba727837a1ed71fd1fa21
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-27 18:37:36 -0500

    setup common needs

---
+++ __needs/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  /* helpers */
+  Server : require ('./Server'),
+  Router : require ('./Router'),
+  /* project map */
+  data   : require ('../data'),
+  models : require ('../models'),
+  routes : require ('../routes'),
+  server : require ('../server'),
+}
