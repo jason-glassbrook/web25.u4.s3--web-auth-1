5d9f98b332a43e82c787945c4557cfee12eb6e66
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-28 16:39:01 -0500

    remove getBy

--- models/users/getBy.js
+++
@@ -1,8 +0,0 @@
-const get = require ('./get')
-
-module.exports =
-  async (key, value, ...rest) => {
-    let [ user_record ] = await get ({ [key] : value }, ...rest)
-
-    return user_record
-  }
