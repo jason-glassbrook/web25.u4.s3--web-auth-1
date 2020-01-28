62c3835b61c964c69f5d346cb584eecc9283f4ec
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-28 12:23:01 -0500

    update var names

--- models/users/push.js
+++ models/users/push.js
@@ -2,10 +2,10 @@ const { data } = require ('./__needs')
 const get = require ('./get')
 
 module.exports =
-  async (user_value) => {
+  async (user_values) => {
     const _ids = await (
       data ('users')
-      .insert (user_value, '_id')
+      .insert (user_values, [ '_id' ])
     )
 
     let user_records = _ids.map (
