14f36343ec91642d91911bff8b6ce3afe85e4f8e
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-28 16:37:30 -0500

    remove [pull,set]By

--- models/users/pullBy.js
+++
@@ -1,8 +0,0 @@
-const pull = require ('./pull')
-
-module.exports =
-  async (key, value, ...rest) => {
-    let [ user_record ] = await pull ({ [key] : value }, ...rest)
-
-    return user_record
-  }
